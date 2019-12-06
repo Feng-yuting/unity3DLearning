4df5d00183437780d90f794bb295639bea56fd55
dongzizhu <473045679@qq.com>
2019-12-06 20:13:16 +0800

    2019-12-6

---
+++ hw9/P&D 动作分离版/Assets/Scripts/UserGUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.Mygame;
+
+public class UserGUI : MonoBehaviour {
+	private IUserAction action;
+	public int status = 0;
+	GUIStyle style;
+	GUIStyle buttonStyle;
+
+	void Start() {
+		action = SSDirector.getInstance ().currentSceneController as IUserAction;
+
+		style = new GUIStyle();
+		style.fontSize = 40;
+		style.alignment = TextAnchor.MiddleCenter;
+
+		buttonStyle = new GUIStyle("button");
+		buttonStyle.fontSize = 30;
+	}
+	void OnGUI() {
+		if (status == 1) { // 输了
+			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-85, 100, 50), "Gameover!", style);
+			if (GUI.Button(new Rect(Screen.width/2-70, Screen.height*2/3, 140, 70), "Restart", buttonStyle)) {
+				status = 0;
+				action.restart ();
+			}
+		} else if(status == 2) { // 赢了
+			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-85, 100, 50), "You win!", style);
+			if (GUI.Button(new Rect(Screen.width/2-70, Screen.height*2/3, 140, 70), "Restart", buttonStyle)) {
+				status = 0;
+				action.restart ();
+			}
+		}
+		else { // 还在玩
+			if (GUI.Button(new Rect(Screen.width/4, Screen.height*2/3, 140, 70), "Row", buttonStyle)) {
+				action.moveBoat();
+			}
+			if (GUI.Button(new Rect(Screen.width*5/9, Screen.height*2/3, 140, 70), "Next", buttonStyle)) {
+				action.autoNext();
+			}
+		}
+	}
+}
