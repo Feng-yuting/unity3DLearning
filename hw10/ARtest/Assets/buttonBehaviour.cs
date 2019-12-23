8fc55fd7f5ba80434a67273b06a0e7decf2834b0
dongzizhu <473045679@qq.com>
2019-12-23 23:23:01 +0800

    2019-12-23

---
+++ hw10/ARtest/Assets/buttonBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class buttonBehaviour : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbs;
+    public Animator animator;
+
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vbs.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Pressed");
+        animator.transform.Translate(Vector3.up /100);
+        
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+        Debug.Log("Released");
+        animator.transform.Translate(Vector3.down / 100);
+    }
+}
+
